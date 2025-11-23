1a4cb6ad291ea201590e6f9e7529390b7db8428b
islam ibrahim <eslam0ebrahem0@gmail.com>
2025-11-23 03:17:12 +0100

    chore: remove smart fields database migration call

--- src/index.js
+++ src/index.js
@@ -22,10 +22,6 @@ cache.init();
 // Connect to MongoDB
 await connectDB();
 
-// Run Smart DB Migration (Phase 7)
-import { migrateSmartFields } from './utils/dbMigration.js';
-migrateSmartFields().catch(err => console.error('Migration error:', err));
-
 // Initialize the bot
 const bot = initializeBot(commands);
 
